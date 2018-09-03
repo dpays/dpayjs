fb6e64ca1112d2394028c049da10a8d538a7a68d
Jared Rice Sr <jaredricelegal@gmail.com>
2018-09-02 19:22:28 -0500

    Finalized dPay.js core to function with dPay network

--- src/formatter.js
+++ src/formatter.js
@@ -1,82 +1,82 @@
 import get from "lodash/get";
 import { key_utils } from "./auth/ecc";
 
-module.exports = steemAPI => {
+module.exports = dpayApi => {
   function numberWithCommas(x) {
     return x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
 
-  function vestingSteem(account, gprops) {
+  function vestingBex(account, gprops) {
     const vests = parseFloat(account.vesting_shares.split(" ")[0]);
     const total_vests = parseFloat(gprops.total_vesting_shares.split(" ")[0]);
-    const total_vest_steem = parseFloat(
-      gprops.total_vesting_fund_steem.split(" ")[0]
+    const total_vest_dpay = parseFloat(
+      gprops.total_vesting_fund_dpay.split(" ")[0]
     );
-    const vesting_steemf = total_vest_steem * (vests / total_vests);
-    return vesting_steemf;
+    const vesting_dpayf = total_vest_dpay * (vests / total_vests);
+    return vesting_dpayf;
   }
 
   function processOrders(open_orders, assetPrecision) {
-    const sbdOrders = !open_orders
+    const bbdOrders = !open_orders
       ? 0
       : open_orders.reduce((o, order) => {
-          if (order.sell_price.base.indexOf("SBD") !== -1) {
+          if (order.sell_price.base.indexOf("BBD") !== -1) {
             o += order.for_sale;
           }
           return o;
         }, 0) / assetPrecision;
 
-    const steemOrders = !open_orders
+    const dpayOrders = !open_orders
       ? 0
       : open_orders.reduce((o, order) => {
-          if (order.sell_price.base.indexOf("STEEM") !== -1) {
+          if (order.sell_price.base.indexOf("BEX") !== -1) {
             o += order.for_sale;
           }
           return o;
         }, 0) / assetPrecision;
 
-    return { steemOrders, sbdOrders };
+    return { dpayOrders, bbdOrders };
   }
 
   function calculateSaving(savings_withdraws) {
     let savings_pending = 0;
-    let savings_sbd_pending = 0;
+    let savings_bbd_pending = 0;
     savings_withdraws.forEach(withdraw => {
       const [amount, asset] = withdraw.amount.split(" ");
-      if (asset === "STEEM") savings_pending += parseFloat(amount);
+      if (asset === "BEX") savings_pending += parseFloat(amount);
       else {
-        if (asset === "SBD") savings_sbd_pending += parseFloat(amount);
+        if (asset === "BBD") savings_bbd_pending += parseFloat(amount);
       }
     });
-    return { savings_pending, savings_sbd_pending };
+    return { savings_pending, savings_bbd_pending };
   }
 
   function estimateAccountValue(
     account,
-    { gprops, feed_price, open_orders, savings_withdraws, vesting_steem } = {}
+    { gprops, feed_price, open_orders, savings_withdraws, vesting_dpay } = {}
   ) {
     const promises = [];
     const username = account.name;
     const assetPrecision = 1000;
     let orders, savings;
 
-    if (!vesting_steem || !feed_price) {
+    if (!vesting_dpay || !feed_price) {
       if (!gprops || !feed_price) {
         promises.push(
-          steemAPI.getStateAsync(`/@{username}`).then(data => {
+          dpayApi.getStateAsync(`/@{username}`).then(data => {
             gprops = data.props;
             feed_price = data.feed_price;
-            vesting_steem = vestingSteem(account, gprops);
+            vesting_dpay = vestingBex(account, gprops);
           })
         );
       } else {
-        vesting_steem = vestingSteem(account, gprops);
+        vesting_dpay = vestingBex(account, gprops);
       }
     }
 
     if (!open_orders) {
       promises.push(
-        steemAPI.getOpenOrdersAsync(username).then(open_orders => {
+        dpayApi.getOpenOrdersAsync(username).then(open_orders => {
           orders = processOrders(open_orders, assetPrecision);
         })
       );
@@ -86,7 +86,7 @@ module.exports = steemAPI => {
 
     if (!savings_withdraws) {
       promises.push(
-        steemAPI
+        dpayApi
           .getSavingsWithdrawFromAsync(username)
           .then(savings_withdraws => {
             savings = calculateSaving(savings_withdraws);
@@ -97,16 +97,16 @@ module.exports = steemAPI => {
     }
 
     return Promise.all(promises).then(() => {
-      let price_per_steem = undefined;
+      let price_per_dpay = undefined;
       const { base, quote } = feed_price;
-      if (/ SBD$/.test(base) && / STEEM$/.test(quote))
-        price_per_steem = parseFloat(base.split(" ")[0]);
+      if (/ BBD$/.test(base) && / BEX/.test(quote))
+        price_per_dpay = parseFloat(base.split(" ")[0]);
       const savings_balance = account.savings_balance;
-      const savings_sbd_balance = account.savings_sbd_balance;
-      const balance_steem = parseFloat(account.balance.split(" ")[0]);
-      const saving_balance_steem = parseFloat(savings_balance.split(" ")[0]);
-      const sbd_balance = parseFloat(account.sbd_balance);
-      const sbd_balance_savings = parseFloat(savings_sbd_balance.split(" ")[0]);
+      const savings_bbd_balance = account.savings_bbd_balance;
+      const balance_dpay = parseFloat(account.balance.split(" ")[0]);
+      const saving_balance_dpay = parseFloat(savings_balance.split(" ")[0]);
+      const bbd_balance = parseFloat(account.bbd_balance);
+      const bbd_balance_savings = parseFloat(savings_bbd_balance.split(" ")[0]);
 
       let conversionValue = 0;
       const currentTime = new Date().getTime();
@@ -123,21 +123,21 @@ module.exports = steemAPI => {
         conversionValue += amount;
       }, []);
 
-      const total_sbd =
-        sbd_balance +
-        sbd_balance_savings +
-        savings.savings_sbd_pending +
-        orders.sbdOrders +
+      const total_bbd =
+        bbd_balance +
+        bbd_balance_savings +
+        savings.savings_bbd_pending +
+        orders.bbdOrders +
         conversionValue;
 
-      const total_steem =
-        vesting_steem +
-        balance_steem +
-        saving_balance_steem +
+      const total_dpay =
+        vesting_dpay +
+        balance_dpay +
+        saving_balance_dpay +
         savings.savings_pending +
-        orders.steemOrders;
+        orders.dpayOrders;
 
-      return (total_steem * price_per_steem + total_sbd).toFixed(2);
+      return (total_dpay * price_per_dpay + total_bbd).toFixed(2);
     });
   }
 
@@ -167,13 +167,13 @@ module.exports = steemAPI => {
       return out;
     },
 
-    vestToSteem: function(
+    vestToBex: function(
       vestingShares,
       totalVestingShares,
-      totalVestingFundSteem
+      totalVestingFundBex
     ) {
       return (
-        parseFloat(totalVestingFundSteem) *
+        parseFloat(totalVestingFundBex) *
         (parseFloat(vestingShares) / parseFloat(totalVestingShares))
       );
     },
@@ -191,7 +191,7 @@ module.exports = steemAPI => {
       return amount.toFixed(3) + " " + asset;
     },
     numberWithCommas,
-    vestingSteem,
+    vestingBex,
     estimateAccountValue,
     createSuggestedPassword
   };
